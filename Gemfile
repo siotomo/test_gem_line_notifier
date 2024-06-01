62d34bd1894528401acb3443b649af27c62f9a11
siotomo <pc.siomi@gmail.com>
2024-06-01 13:49:36 +0900

    initialCommit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in test_gem_line_notifier.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/test_gem_line_notifier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "test_gem_line_notifier/version"
+
+module TestGemLineNotifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
